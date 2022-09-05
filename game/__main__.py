9e8dddbf6160cc5cedc4f4f62517219ebbc26b72
Jayandjeff <jaydeniqv2@gmail.com>
2022-09-04 18:28:07 -0700

    Added player class, fixed relative imports

--- game/__main__.py
+++ game/__main__.py
@@ -2,41 +2,49 @@
 from sys import exit
 
 # Constants
-from constants import (
+from .constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
     FPS,
+    BACKGROUND
 )
+
+from .player import Player
 
 # Downloaded - Module
 import pygame
 
-pygame.init()
-
-# Window
-
-win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#pygame.display.set_icon("")
-pygame.display.set_caption("Theme: The Red Planet!")
-
-clock = pygame.time.Clock()
-
 # Main Function
 def main():
+    pygame.init()
+
+    # Window
+
+    win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #pygame.display.set_icon("")
+    pygame.display.set_caption("Theme: The Red Planet!")
+
+    clock = pygame.time.Clock()
+    
+    player = Player(500, 360)
 
     run = True
     while run:
-        clock.tick(FPS)
-    
+        pygame.event.pump()
+        
+        win.fill(BACKGROUND)
+        player.update(pygame.sprite.Group())
+        player.draw(win)
+        pygame.display.flip()
         # Ends Program when the X button is clicked
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
     
-
+        clock.tick(FPS)
         # Updates Window
-        pygame.display.update()
+        
 
     # Closes Program
     pygame.quit()
